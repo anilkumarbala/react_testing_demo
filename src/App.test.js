7f8674a87b8459d78c99ab363490366cacb939a9
Bala Anil Kumar Yarravarapu -X (byarrava - TECH MAHINDRA LIM at Cisco) <byarrava@cisco.com>
2023-10-02 20:53:40 +0530

    second test cases regarding button and check with disable and enable functionalities

--- src/App.test.js
+++ src/App.test.js
@@ -16,4 +16,22 @@ test('button turns blue when clicked', () => {
   expect(colorButton).toHaveTextContent('Change to red')
 
 });
+test('initial conditions', () => {
+  render(<App/>);
+  const colorButton=screen.getByRole('button',{name:'Change to blue'});
+  expect(colorButton).toBeEnabled();
+  const checkbox=screen.getByRole('checkbox');
+  expect(checkbox).not.toBeChecked();
+
+});
+test('checkbox disables button on first click and enables on second click',()=>{
+  render(<App/>);
+  const checkbox=screen.getByRole('checkbox');
+  const button=screen.getByRole('button');
+  fireEvent.click(checkbox);
+  expect(button).toBeDisabled();
+  fireEvent.click(checkbox);
+  expect(button).toBeEnabled();
+
+})
 
